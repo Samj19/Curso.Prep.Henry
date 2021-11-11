3b476556b2a6118771db55464139c1b307c36209
Samuel Culpa <culpasamuel@gmail.com>
2021-11-10 22:13:04 -0400

    finalizado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -281,7 +281,7 @@ function esVocal(letra){
   
   if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u')
    {
-     return 'Es Vocal';
+     return 'Es vocal';
   }
     else 
     {
