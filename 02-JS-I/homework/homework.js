6bf4dabdc73ad8824674045c5685ea55a46b6e62
Samuel Culpa <culpasamuel@gmail.com>
2021-11-10 22:10:25 -0400

    final

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -232,7 +232,7 @@ function combinarNombres(nombre, apellido) {
 }
 
 function obtenerSaludo(nombre) {
-  return ('Hola'+ nombre);
+  return ('Hola'+ ' ' + nombre);
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
@@ -281,7 +281,7 @@ function esVocal(letra){
   
   if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u')
    {
-     return 'Es una Vocal';
+     return 'Es Vocal';
   }
     else 
     {
