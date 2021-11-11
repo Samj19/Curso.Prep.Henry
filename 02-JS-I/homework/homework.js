93b12bbc4a739e6f71b4eccde96edc205fad9c03
Samuel Culpa <culpasamuel@gmail.com>
2021-11-10 21:29:08 -0400

    modoficacion final

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -217,12 +217,14 @@ function esPositivo(numero) {
 }
 
 function agregarSimboloExclamacion(str) {
+  return str + '!'; 
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
 }
 
 function combinarNombres(nombre, apellido) {
+  return nombre+' '+apellido; 
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
@@ -230,6 +232,7 @@ function combinarNombres(nombre, apellido) {
 }
 
 function obtenerSaludo(nombre) {
+  return 'Hola'+nombre;
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
@@ -237,6 +240,7 @@ function obtenerSaludo(nombre) {
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
+  return alto * ancho;
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
   
@@ -244,6 +248,7 @@ function obtenerAreaRectangulo(alto, ancho) {
 
 
 function retornarPerimetro(lado){
+  return lado * 4;
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
   
@@ -251,6 +256,7 @@ function retornarPerimetro(lado){
 
 
 function areaDelTriangulo(base, altura){
+  return (base * altura) / 2;
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
 
@@ -258,6 +264,8 @@ function areaDelTriangulo(base, altura){
 
 
 function deEuroAdolar(euro){
+
+  return euro * 1.20;
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
@@ -266,6 +274,20 @@ function deEuroAdolar(euro){
 
 
 function esVocal(letra){
+
+  if (letra.lenght > 1) {
+    return 'Dato Incorrecto';
+  }
+  
+  if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u')
+   {
+     return 'Es una Vocal';
+  }
+    else 
+    {
+      return 'Dato incorrecto';
+  }
+  
   //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
   //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
