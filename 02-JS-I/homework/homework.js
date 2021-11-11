77662b3b100a2ac166fc6aa70fce66cb46277aa9
Samuel Culpa <culpasamuel@gmail.com>
2021-11-10 22:11:43 -0400

    final2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -232,7 +232,7 @@ function combinarNombres(nombre, apellido) {
 }
 
 function obtenerSaludo(nombre) {
-  return ('Hola'+ ' ' + nombre);
+  return ('Hola'+ ' ' + nombre + '!');
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
